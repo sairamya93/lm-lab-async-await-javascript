37a4f0ba71bb133c12573edaaa9c911666e28e8b
Ramya <123948635+sairamya93@users.noreply.github.com>
2024-02-09 10:41:35 +0000

    Updated solution in exercise002.js with async/await

--- exercise001.js
+++ exercise001.js
@@ -1,15 +1,14 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-		
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
 
 // Your solution(s) to exercise001 here!
@@ -36,14 +35,15 @@ promise.then((response) => {
 // }
 
 const getPromiseResponse = async () => {
-	try {
-		const result = await promise
-		console.log(`Yay! Promise resolved with response: ${result}`);
-	} catch(error) {
-	console.error(`Boo. Promise rejected with response: ${error}`);
-}
-}
+  try {
+        const result = await promise;
+        console.log(`Yay! Promise resolved with response: ${result}`);
+      } 
+	  catch (error) 
+	  {
+      console.error(`Boo. Promise rejected with response: ${error}`);
+      }
+}; 
 
 //calling the async function
-getPromiseResponse()
-
+getPromiseResponse();
